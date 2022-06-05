c359c5cf34f8c08dc17471e195abaf0a217918da
CharlotteMaschke <55599742+CharlotteMaschke@users.noreply.github.com>
2022-06-05 16:05:11 -0400

    add array job

--- jobs_step1_test.sh
+++ jobs_step1_test.sh
@@ -8,16 +8,15 @@
 #SBATCH --cpus-per-task=2
 #SBATCH --mail-user=q2h3s6p4k0e9o7a5@biaptlab.slack.com # adjust this to match your email address
 #SBATCH --mail-type=ALL
-#SBATCH --array=WSAS02, WSAS09, WSAS20, WSAS27, WSAS28, WSAS10, WSAS11, WSAS12, WSAS13, WSAS17, WSAS18, WSAS22, WSAS25, WSAS29, WSAS05, WSAS19
-
+#SBATCH --array=1,2
 
 module load python/3.9.6
 module load scipy-stack
 source dimred_env/bin/activate
 
 python -u 1_functional_connectivity.py \
-       -input_dir ~/Dim_Red/Dimensionality_reduction/DATA \
-	   	 -output_dir ~/Dim_Red/Dimensionality_reduction/RESULTS \
-		   -participants ~/Dim_Red/Dimensionality_reduction/DATA/data_2states.txt \
+       -input_dir ~/projects/def-sblain/lotte/Dim_Red/Dimensionality_reduction/DATA \
+       -output_dir ~/projects/def-sblain/lotte/Dim_Red/Dimensionality_reduction/RESULTS \
+       -participants ~/projects/def-sblain/lotte/Dim_Red/Dimensionality_reduction/DATA/data_2states.txt \
 		   -frequencyband alpha \
 		   -id ${SLURM_ARRAY_TASK_ID}
